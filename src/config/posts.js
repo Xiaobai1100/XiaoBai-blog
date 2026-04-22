7d4f2d9d9cb147eda8455dad4c05c8e1b38137c6
Bai <XiaoBai_1100@outlook.com>
2026-04-22 17:01:45 +0800

    feat: publish continuous dynamics log and update post config

--- src/config/posts.js
+++ src/config/posts.js
@@ -1,9 +1,20 @@
 import AboutBlog from '../pages/AboutBlog'; 
 import ChristmasLog from '../pages/ChristmasLog';
 import ChaosLog_1 from '../pages/ChaosLog_1';
+import ChaosLog_2 from '../pages/ChaosLog_2';
+
+
 
 export const POSTS = [
- 
+{
+id: 'continuous-dynamics', // 新路由的 URL ID (如 /continuous-dynamics)
+title: 'CONTINUOUS_DYNAMICS: DIFFERENTIAL_EQUATIONS_&_VECTOR_FIELDS',
+category: 'RESEARCH',
+date: '2026-04-22',
+description: 'Transitioning from discrete maps to continuous differential equations, exploring vector fields and bifurcations.',
+component: ChaosLog_2 // 关联到刚刚导入的组件
+},
+
 {
 id: 'chaos-dynamics', // 路由使用的 ID
 title: 'SIGNAL_RECEIVED: DYNAMICS_FIXED_POINTS_TO_CHAOS',
